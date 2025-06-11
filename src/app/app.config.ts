e078c2ef375fd6b4b2958c9293e8a212de586053
Taosif <mdtaosifulislam@gmail.com>
2025-06-11 16:14:15 +0600

    added firestone module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient } from '@angular/common/http';
+
 import { routes } from './app.routes';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCKi6E4z8wrJlW5Mi-VXAecYFOUNry6s-M',
+    authDomain: 'khush-bunon.firebaseapp.com',
+    projectId: 'khush-bunon',
+    storageBucket: 'khush-bunon.firebasestorage.app',
+    messagingSenderId: '478590256558',
+    appId: '1:478590256558:web:bba589f4c9db748995b43f',
+    measurementId: 'G-QDDZ7TC36M',
+  },
+};
