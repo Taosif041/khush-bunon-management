c42d2ab122935aea2f913def85d1915a589ac535
Taosif <mdtaosifulislam@gmail.com>
2025-06-11 16:48:34 +0600

    updated to angular 19

--- src/app/components/Users/userlist/userlist.component.ts
+++ src/app/components/Users/userlist/userlist.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-userlist',
-  standalone: true,
-  imports: [],
-  templateUrl: './userlist.component.html',
-  styleUrl: './userlist.component.css'
+    selector: 'app-userlist',
+    imports: [],
+    templateUrl: './userlist.component.html',
+    styleUrl: './userlist.component.css'
 })
 export class UserlistComponent {
 
